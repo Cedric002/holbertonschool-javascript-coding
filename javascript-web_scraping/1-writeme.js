c5fd4db8251eeca7d23851d77039bae579d0fa95
Cedric002 <cedgnleblanc@gmail.com>
2024-05-13 15:30:00 +0200

    fix 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-fs.writeFile(process.argv[2], process.argv[3], "utf-8", (err) => {
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, "utf-8", (err) => {
   if (err) {
-    console.error("Error writing file:", err);
-  } else {
-    console.log("File written successfully!");
+    console.log;
   }
 });
