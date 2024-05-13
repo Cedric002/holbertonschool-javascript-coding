23c2c515dda11f046cdc48ad1c7b8336355bb1d4
Cedric002 <cedgnleblanc@gmail.com>
2024-05-13 12:39:06 +0200

    fix 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const fs = require("fs");
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, "utf-8", (err, data) => {
+fs.readFile(process.argv[2], "utf-8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
