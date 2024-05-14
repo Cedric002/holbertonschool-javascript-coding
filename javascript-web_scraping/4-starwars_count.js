b7ac1d2d4b6a9e23a6ce55613e6c7321f1b25828
Cedric002 <cedgnleblanc@gmail.com>
2024-05-14 10:34:27 +0200

    fix 2  4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,31 +1,19 @@
 #!/usr/bin/node
 const request = require("request");
 
-if (process.argv.length !== 3) {
-  console.log("Usage: node script.js <api_url>");
-  process.exit(1);
-}
+let url = process.argv[2];
 
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`An error occurred: ${error.message}`);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    const moviesWithCharacter = films.filter((film) => {
-      const characterUrls = film.characters;
-      return characterUrls.includes(
-        `https://swapi-api.hbtn.io/api/people/${characterId}/`
-      );
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    let films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes("/18/")) {
+          count++;
+        }
+      });
     });
-
-    console.log(moviesWithCharacter.length);
-  } else {
-    console.log(`Error: ${response.statusCode}`);
+    console.log(count);
   }
 });
