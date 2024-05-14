b18a2688505782604db7909caece8692e3958165
Cedric002 <cedgnleblanc@gmail.com>
2024-05-14 10:26:25 +0200

    fix 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,19 +9,21 @@ if (process.argv.length !== 3) {
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request(`${apiUrl}`, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`An error occurred: ${error.message}`);
     return;
   }
 
   if (response.statusCode === 200) {
-    const movies = JSON.parse(body).results;
-    const moviesWithCharacter = movies.filter((movie) =>
-      movie.characters.includes(
+    const films = JSON.parse(body).results;
+    const moviesWithCharacter = films.filter((film) => {
+      const characterUrls = film.characters;
+      return characterUrls.includes(
         `https://swapi-api.hbtn.io/api/people/${characterId}/`
-      )
-    );
+      );
+    });
+
     console.log(moviesWithCharacter.length);
   } else {
     console.log(`Error: ${response.statusCode}`);
