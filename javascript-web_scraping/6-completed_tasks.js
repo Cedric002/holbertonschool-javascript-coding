d5b96e17be6e11d4ac097c0a4dc04d98e13eeee2
Cedric002 <cedgnleblanc@gmail.com>
2024-05-15 10:31:18 +0200

    fix 7  6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,35 +1,21 @@
 #!/usr/bin/node
 const request = require("request");
+const url = process.argv[2];
 
-// Faites une requête à l'API
-request(
-  "https://jsonplaceholder.typicode.com/todos",
-  (error, response, body) => {
-    if (error) {
-      console.error("Erreur lors de la requête :", error.message);
-      return;
-    }
-
-    try {
-      const todos = JSON.parse(body);
-
-      // Créez un objet pour stocker le nombre de tâches terminées par utilisateur
-      const completedTasks = {};
-
-      todos.forEach((task) => {
-        if (task.completed) {
-          // Incrémente le compteur pour l'identifiant d'utilisateur
-          completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
-        }
-      });
-
-      // Affichez les résultats sous forme d'objet
-      console.log(completedTasks);
-    } catch (parseError) {
-      console.error(
-        "Erreur lors de l'analyse des données de l'API :",
-        parseError.message
-      );
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let tasks = JSON.parse(body);
+  let obj = {};
+  for (let task of tasks) {
+    if (task.completed === true) {
+      if (obj[task.userId] === undefined) {
+        obj[task.userId] = 1;
+      } else {
+        obj[task.userId]++;
+      }
     }
   }
-);
+  console.log(obj);
+});
