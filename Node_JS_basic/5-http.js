8a621e419883fb91c7bce6e028210224780bb0fc
Cedric002 <cedgnleblanc@gmail.com>
2024-05-27 14:54:12 +0200

    fix 2  5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,14 +1,14 @@
-import http from "http";
-import fs from "fs";
-import util from "util";
+import http from 'http';
+import fs from 'fs';
+import util from 'util';
 
 const readFile = util.promisify(fs.readFile);
 
 async function readDatabase(filePath) {
   try {
-    const data = await readFile(filePath, "utf8");
-    const lines = data.split("\n");
-    const students = lines.map((line) => line.split(","));
+    const data = await readFile(filePath, 'utf8');
+    const lines = data.split('\n');
+    const students = lines.map(line => line.split(','));
     return students;
   } catch (error) {
     throw error;
@@ -16,18 +16,18 @@ async function readDatabase(filePath) {
 }
 
 const app = http.createServer(async (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  if (req.url === "/") {
-    res.write("Hello Holberton School!");
-  } else if (req.url === "/students") {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
     try {
-      const students = await readDatabase("/path/to/database.csv");
-      res.write("This is the list of our students\n");
-      students.forEach((student) => {
+      const students = await readDatabase('/path/to/database.csv');
+      res.write('This is the list of our students\n');
+      students.forEach(student => {
         res.write(`Name: ${student[0]}, Field: ${student[3]}\n`);
       });
     } catch (error) {
-      res.write("Cannot load the database");
+      res.write('Cannot load the database');
     }
   }
   res.end();
